ae87fce85ee7d66fbd3e44235d58e4d9372db877
Muhammad Shahwaiz <mshahwaiz12@gmail.com>
2024-10-12 02:45:52 +0500

    ranking ui panels + panel entries prefabs + panel switching from sidebar

--- Assets/Project T/Scripts/UI Panels/Rankings/TeamPanel.cs
+++ Assets/Project T/Scripts/UI Panels/Rankings/TeamPanel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Scripts.Resources;
 using UnityEngine;
 
-public class RankingsUI : MonoBehaviour
+public class TeamPanel : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -20,6 +20,5 @@ public class RankingsUI : MonoBehaviour
     public async void printToConsole() {
         Debug.Log("this is a console test.");
         await AppConstants.instance.GetAllRounds();
-        AppConstants.instance.PrintRankings();
     }
 }
